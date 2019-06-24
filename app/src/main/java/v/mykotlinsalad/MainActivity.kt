9308e14909f40db40643d8fbfa2b74204125ae4a
vexild <vili.ahonen@hotmail.com>
2019-06-24 15:38:21 +0300

    init push

---
+++ app/src/main/java/v/mykotlinsalad/MainActivity.kt
@@ -0,0 +1,18 @@
+package v.mykotlinsalad
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun openWeather(view: View){
+        val intent = Intent(this, forecastActivity::class.java)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/v/mykotlinsalad/forecastActivity.kt
@@ -0,0 +1,85 @@
+package v.mykotlinsalad
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_forecast.*
+import org.json.JSONObject
+
+
+class forecastActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forecast)
+
+        val apiKey : String = "bff78d1e25c341ad884080598125bf67"
+        // get json
+        getJson(apiKey, "Oulu")
+    }
+
+    /*
+    fun getLocation(): String {
+        val targetLocation = "Oulu"
+        return targetLocation
+    }*/
+
+    fun updateLocation(view: View){
+        val newCity = new_city_text.text.toString()
+        location.text = ""
+        temperature.text = ""
+        humidity.text = ""
+        status.text = ""
+        val apiKey = "bff78d1e25c341ad884080598125bf67"
+        getJson(apiKey,newCity)
+    }
+
+
+    protected fun getJson(apiKey: Any?, targetLocation: String) {
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.openweathermap.org/data/2.5/weather?q="+targetLocation+"&appid="+apiKey
+        println("Used url: "+url)
+        println("target location: "+ targetLocation)
+
+        val stringRequest = StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
+
+            // get location:
+            val mainObject = JSONObject(response)
+            println(mainObject.toString())
+            val location_string = mainObject.getString("name")
+            println("Location: "+location_string)
+            location.text = location_string.toString()
+
+            // get temperature
+            val temp_result = JSONObject(response).getJSONObject("main")
+            val temp_kelvin = temp_result.getString("temp").toDouble()
+            println("faren: "+temp_kelvin)
+            val temp_cels = (temp_kelvin - 273.15)
+            temperature.text = (temp_cels.toString()).substring(0,4)+" C"+176.toChar()
+
+            // get humidity
+            val humidity_string = temp_result.getString("humidity").toFloat()
+            println("humidity: "+ humidity_string)
+            humidity.text = (humidity_string.toString()+"%")
+
+            // get status
+            val weather_status =  JSONObject(response).getJSONArray("weather").getJSONObject(0).getString("description")
+
+            status.text = weather_status
+        },
+            Response.ErrorListener { temperature.text = "That didn't work!" })
+        queue.add(stringRequest)
+    }
+
+
+    fun close(view: View){
+        finish()
+    }
+}
+
+

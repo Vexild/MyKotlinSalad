585184dba8e8e03fb27cf69bad913b02df96a08f
vexild <vili.ahonen@hotmail.com>
2019-06-26 15:25:50 +0300

    Added google mapveiw to GPSLocation activity. Also fixed database and now data gets saved

--- app/src/main/java/v/mykotlinsalad/GPSLocationActivity.kt
+++ app/src/main/java/v/mykotlinsalad/GPSLocationActivity.kt
@@ -12,12 +12,20 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_gpslocation.*
 import org.json.JSONObject
 
-class GPSLocationActivity : AppCompatActivity() {
+class GPSLocationActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    var myDb = databaseHelper(this)
+
+
+    var myDb = databaseHelper(this, null)
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     val apiKey = "bff78d1e25c341ad884080598125bf67"
     var lon: Double = 0.0
@@ -26,24 +34,43 @@ class GPSLocationActivity : AppCompatActivity() {
     var cityName: String? = ""
     var country: String? = ""
 
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        Log.i("onMapRedu", "Function start")
+        mMap = googleMap
+
+        val startingPoint = LatLng(65.009009009009, 25.50388924350585)
+        /*
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startingPoint, 5.5.toFloat()))
+    }
+
+
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gpslocation)
-
+        Log.i("onCreate In Maps", "Checkpoint1")
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapView2) as SupportMapFragment
+        Log.i("onCreate In Maps", "Checkpoint2")
+        mapFragment.getMapAsync(this)
         // create local db if none exists and CREATE_TABLE = "CREATE TABLE $LOCATIONS ($ID Integer PRIMARY KEY AUTOINCREMENT, $LON, $LAT, $CITY, $COUNTRY)"
-
 
     }
     @SuppressLint("MissingPermission")
     fun search(view: View){
+
+        Log.i("SearchButton","Search pressed")
+        searchGPSlocationButton.isClickable= false
         // CREATE PERMISSION FOR USE OF GPS
         // in current build, you need to allow myKotlinSalad use GPS manually (settings -> search myKotlinSalad -> permissions -> gps toggle on)
 
         // create all mandatory things for GPS location
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-
-
         fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
 
 
@@ -54,11 +81,12 @@ class GPSLocationActivity : AppCompatActivity() {
                 lat = location.latitude.toDouble()
             }
         }
-        // get the city name
-        while (lat != 0.0 && lon != 0.0) {
+
+        fusedLocationClient.lastLocation.addOnCompleteListener {
+            // get the city name
             val queue = Volley.newRequestQueue(this)
             val url =
-                "https://api.openweathermap.org/data/2.5/weather?lat=" + lat.toInt() + "&lon=" + lon.toInt() + "&appid=" + apiKey
+                "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey
 
 
             Log.d("Lon form GPSLocation", lon.toString())
@@ -66,23 +94,48 @@ class GPSLocationActivity : AppCompatActivity() {
             val stringRequest = StringRequest(Request.Method.GET, url, Response.Listener<String> { response ->
 
                 cityName = JSONObject(response).getString("name").toString()
-                gpsDescription.text = JSONObject(response).toString()
-                country = Log.i("JSON", JSONObject(response).getJSONObject("sys").getString("country").toString()).toString()
+                //gpsDescription.text = JSONObject(response).toString()
+                country = JSONObject(response).getJSONObject("sys").getString("country").toString()
 
                 Log.i("Location City: ", cityName)
                 Log.i("Location Country: ", country)
-            }, Response.ErrorListener { Log.e("GPS", "That didn't work!") })
+                val NewMarker = LatLng(lat,lon)
+                mMap.addMarker(MarkerOptions().position(NewMarker).title("Marker in "+cityName.toString()+", "+country.toString()))
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(NewMarker))
+
+            }, Response.ErrorListener { Log.e("GPS", "Error in GPSLocationActivity") })
             queue.add(stringRequest)
 
-            break
+            // display all points from db to map
+            // Add a marker in Sydney and move the camera
+
+
+
+
+            // take lon and lat and insert them into DB in form (id, lon, lat, city)7
+            //myDb.deleteAllCoordinates()
+            val data = Coordinates(lat,lon,cityName.toString(), country.toString())
+            myDb.insertCoordinates(data)
+
+
+            val cursor = myDb.getAllCoordinates()
+            cursor!!.moveToFirst()
+            //gpsDescription.append((cursor.getString(cursor.getColumnIndex(databaseHelper.ID))))
+            while (cursor.moveToNext()) {
+                myDb.deleteAllCoordinates()
+
+               // Log.i("DATABASE"," LATTITUDE: " + cursor.getString(cursor.getColumnIndex(databaseHelper.ID)))
+                //Log.i("DATABASE"," LONGITUDE: " + cursor.getString(cursor.getColumnIndex(databaseHelper.LON)))
+                val response = "LATITUDE: " + cursor.getString(1)+", LONGITUDE: "+ cursor.getString(2)+" CITY: "+ cursor.getString(3).toString()+" COUNTRY: "+cursor.getString(4).toString()
+                Log.i("DATABASE"," LATITUDE: " + cursor.getString(1)+", LONGITUDE: "+ cursor.getString(2)+" CITY: "+ cursor.getString(3).toString()+" COUNTRY: "+cursor.getString(4).toString())
+                gpsTitle.text = response
+            }
+            cursor.close()
+        }
+        searchGPSlocationButton.isClickable= true
         }
 
-        // take lon and lat and insert them into DB in form (id, lon, lat, city)
-        myDb.insertCoordinates(lon,lat,cityName.toString(), country.toString() )
-        gpsDescription.text = (myDb.getAllCoordinates()).toString()
-        // display all points from db to map
-
-
-        }
 }
+
+
 
--- app/src/main/java/v/mykotlinsalad/databaseHelper.kt
+++ app/src/main/java/v/mykotlinsalad/databaseHelper.kt
@@ -6,45 +6,64 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
-class databaseHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
+class databaseHelper(context: Context, factory: SQLiteDatabase.CursorFactory?): SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
 
     companion object{
         val DATABASE_NAME = "locationDB.db"
         val TABLE_NAME = "location"
         val ID = "ID"
-        val LON = "LON"
-        val LAT = "LAT"
-        val CITY = "CITY"
-        val COUNTRY = "COUNTRY"
+        val LON = "lon"
+        val LAT = "lat"
+        val CITY = "city"
+        val COUNTRY = "country"
+        val DATABASE_VERSION = 1
     }
 
-    override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL("CREATE TABLE IF NOT EXISTS $TABLE_NAME(ID INTEGER PRIMARY KEY AUTOINCREMENT, LON DOUBLE, LAT DOUBLE, CITY TEXT, COUNTRY TEXT)")
+    override fun onCreate(db: SQLiteDatabase) {
+        //db?.execSQL("CREATE TABLE IF NOT EXISTS $TABLE_NAME(ID INTEGER PRIMARY KEY AUTOINCREMENT, LON DOUBLE, LAT DOUBLE, CITY TEXT, COUNTRY TEXT)")
+        val CREATE_PRODUCTS_TABLE = ( "CREATE TABLE IF NOT EXISTS "+TABLE_NAME+"(ID INTEGER PRIMARY KEY AUTOINCREMENT, lon DOUBLE, lat DOUBLE, city TEXT, country TEXT)" )
+        db.execSQL(CREATE_PRODUCTS_TABLE)
     }
-    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
-        db?.execSQL("DROP TABLE IF EXISTS $TABLE_NAME")
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS "+ TABLE_NAME)
         onCreate(db)
     }
 
-    fun insertCoordinates(lon: Double, lat: Double, city: String, country: String): Boolean?{
+    fun insertCoordinates(coordinate: Coordinates){
         val db = this.writableDatabase
         val cv = ContentValues()
-        cv.put(LON, lon)
-        cv.put(LAT, lat)
-        cv.put(CITY, city)
-        cv.put(COUNTRY, country)
-        val res = db.insert(TABLE_NAME,null, cv)
-        return !res.equals(-1)
+        cv.put(LON, coordinate.lon)
+        cv.put(LAT, coordinate.lat)
+        cv.put(CITY, coordinate.city)
+        cv.put(COUNTRY, coordinate.country)
+        db.insert(TABLE_NAME,null, cv)
+        db.close()
     }
 
-    fun getAllCoordinates(): Cursor{
+    fun getAllCoordinates(): Cursor? {
         val db = this.writableDatabase
-        return db.rawQuery("SELECT LON, LAT FROM $TABLE_NAME", null)
+        return db.rawQuery("SELECT * FROM $TABLE_NAME", null)
     }
 
-    fun deleteCoordinates(id: String): Int? {
+
+
+    fun deleteAllCoordinates(): Int {
         val db = this.writableDatabase
-        return db.delete(TABLE_NAME, "ID =? ", arrayOf(id))
+        return db.delete("$TABLE_NAME",null,null)
+        //return db.rawQuery ("DELETE FROM $TABLE_NAME", null)
+    }
+
+}
+class Coordinates {
+    var lat: Double? = null
+    var lon: Double? = null
+    var city: String? = null
+    var country: String? = null
+    constructor(lat: Double, lon: Double, city: String, country: String) {
+        this.lat = lat
+        this.lon = lon
+        this.city = city
+        this.country = country
     }
 
 }
